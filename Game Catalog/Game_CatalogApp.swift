fd76127ffe415ad0eb69af842668c53b3746a94e
Danang Wijaya <danangwijaya750@gmail.com>
2021-08-11 19:18:21 +0700

    Initial Commit

---
+++ Game Catalog/Game_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalogApp.swift
+//  Game Catalog
+//
+//  Created by Danang Wijaya on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
